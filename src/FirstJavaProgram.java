fbc3e51da70ab9756494b9fe7c836c3da71bad9d
100147919 <100147919@lubbockisd.net>
2023-08-30 09:23:47 -0500

    Initial Attempt

--- src/FirstJavaProgram.java
+++ src/FirstJavaProgram.java
@@ -1,5 +1,10 @@
-public class FirstJavaProgram {
+public class FirstJavaProgram { //this is a comment to describe what things do
+    //comments can go anywhere
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print("How's it\t going\b"); // \n creates a new line between two phrases in print
+// \t creates a tab between print
+        //b backspaces one character
+
     }
 }
