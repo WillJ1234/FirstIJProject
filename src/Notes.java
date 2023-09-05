fc71760ff79c97aafaef72886c074478bbce2abe
100147919 <100147919@lubbockisd.net>
2023-09-05 09:03:40 -0500

    Notes on Variables and Data types

---
+++ src/Notes.java
@@ -0,0 +1,26 @@
+public class Notes { //this is a comment to describe what things do
+    //comments can go anywhere
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.print("How's it\t going\b"); // \n creates a new line between two phrases in print
+// \t creates a tab between print
+        //b backspaces one character
+
+        //VARIABLES
+     //Data types are IMPORTANT
+        //Identifier Rules
+            //-- always start with a letter, no spaces, Can use numbers, _ is the only symbol, CANNOT use JAVA keywords
+        //General Conventions
+            // Classes begin with a Capitol, camelCase is common for variableNames, use names that mean something
+
+    //DATA TYPES
+        // -primatives store a single, simple value
+        // 8 types: 4 integers, 2 real numbers, and 1 boolean (t/f), 1 char
+        //-- Integers: byte, short, int, long (Difference is size in bytes)
+        //     bits:     8      16   32   64
+        // bytes: -128 - 127
+        // short: j-32768 - 32767
+        // int: -2147483648 - 2147483647
+        // long: -4quintillion to 4quintillion
+    }
+}
